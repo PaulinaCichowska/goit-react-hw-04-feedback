9dea288ca113a3b8bc019ccc10f035ae39ffbbb4
PaulinaCichowska <Paulina_Cichowska@hotmail.com>
2023-10-19 19:06:49 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,27 +3,39 @@ import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions'
 import React, { Component } from 'react';
 
 export class App extends Component {
+  static defaultProps = {
+    step: 1,
+  }
+
   state = {
     good: 0,
     neutral: 0,
     bad: 0
   }
 
+  handleIncrement = (event) => {
+
+    this.setState(prevState => {
+      console.log(prevState.value);// zwróci poprawne wartości stanu podczas każdej iteracji
+
+      return { value: prevState.value + 1 };
+    });
+  }
 
   render() {
     return <>
       <div>
         <h2>Please leave feedback </h2>
-        <input type="button" value={"good"} />
-        <input type="button" value={"neutral"} />
-        <input type="button" value={"bad"} />
+        <input name="good" onClick={this.handleIncrement} type="button" value={this.state.good} />
+        <input name="neutral" onClick={this.handleIncrement} type="button" value={"neutral"} />
+        <input name="bad" onClick={this.handleIncrement} type="button" value={"bad"} />
       </div>;
       <div>
         <h2>Statistics</h2>
         <ul>
-          <li>good</li>
-          <li>Neutral</li>
-          <li>Bad</li>
+          <li>good {this.state.good}</li>
+          <li>Neutral {this.state.neutral}</li>
+          <li>Bad {this.state.bad}</li>
           <li>Total</li>
           <li>Positive feedback </li>
         </ul>
